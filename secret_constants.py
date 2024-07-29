4a1f74c80b5fb0500ee5d9edde110d5a637348c1
Ian Walsh <iamwalsh@icloud.com>
2024-07-29 16:57:43 -0400

    Create secret_constants.py

---
+++ secret_constants.py
@@ -0,0 +1,5 @@
+CANVAS_API_TOKEN = "..."
+CANVAS_API_URL = "https://canvas.olin.edu/api/v1"
+
+GPT_API_TOKEN = "..."
+GPT_API_URL = "https://call-next-wave.openai.azure.com/openai/deployments/ISIM_Autograder/chat/completions?api-version=2024-02-15-preview"
